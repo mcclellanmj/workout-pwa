537cface0806ddd44257e98f067db21749c55ac5
Matt McClellan <mcclellan.mj@gmail.com>
2019-04-04 21:08:24 -0500

    Improving the pwa functionality

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,9 +1,36 @@
-const version = 20190401;
+const version = "v1.0.0";
+
+const precache = [
+  './',
+  './index.html',
+  './pages/workout/',
+  './pages/workout/index.html',
+  './pages/workout/workout.js',
+  './javascript/element-utils.js',
+  './images/icon.png',
+  './css/main.css',
+  './components/timer.js',
+]
 
 addEventListener('install', event => {
-    //console.log("Installed");
+  event.waitUntil((async () => {
+    const cache = await caches.open(version);
+
+    await cache.addAll(precache);
+  })());
+});
+
+
+addEventListener('activate', event => {
+  event.waitUntil((async () => {
+    for (const cacheName of await caches.keys()) {
+      if (version != cacheName) await caches.delete(cacheName);
+    }
+  })());
 });
 
 addEventListener('fetch', event => {
-    event.respondWith(fetch(event.request));
+   event.respondWith(
+     caches.match(event.request).then(r => r || fetch(event.request))
+ );
 });
